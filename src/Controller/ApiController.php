20b3f463e9e4fb928e356c30eccc361712f5892e
chaima garoui <chaimagaroui2@gmail.com>
2024-08-19 15:29:09 +0200

    ajout : style des users (css)

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -2,17 +2,31 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\HttpClient\ApiHttpClient;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class ApiController extends AbstractController
 {
+    private $apiHttpClient;
+
+    public function __construct(ApiHttpClient $apiHttpClient)
+    {
+        $this->apiHttpClient = $apiHttpClient;
+    }
+
+
     #[Route('/api', name: 'app_api')]
     public function index(): Response
     {
+        // Utilisation de la méthode getUsers() pour obtenir les utilisateurs
+        $users = $this->apiHttpClient->getUsers();
+
+        // Passer les utilisateurs au template Twig
         return $this->render('api/index.html.twig', [
             'controller_name' => 'ApiController',
+            'users' => $users,
         ]);
     }
 }
